eff9cf2fd62928b12302c9541a4ec69a91b4d3c1
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-15 23:18:21 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -17,25 +17,27 @@ import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <BrowserRouter>
         <UserStorage>
           <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
-            <Route
-              path="/account/*"
-              element={
-                <ProtectedRoute>
-                  <User />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/photo/:id" element={<Photo />} />
-            <Route path="/profile/:user" element={<UserProfile />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login/*" element={<Login />} />
+              <Route
+                path="/account/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/photo/:id" element={<Photo />} />
+              <Route path="/profile/:user" element={<UserProfile />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
           <Footer />
         </UserStorage>
       </BrowserRouter>
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,15 @@ import React from "react";
 
 import styles from "./Footer.module.css";
 
+import { ReactComponent as Dogs } from "../../Assets/dogs-footer.svg";
+
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs &copy; Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
